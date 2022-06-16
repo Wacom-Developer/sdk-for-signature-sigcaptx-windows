b8635b2185a7922ab71008a216b86c0dc59035ee
sigsupport <55099853+sigsupport@users.noreply.github.com>
2022-06-16 11:15:27 +0100

    Add files via upload

---
+++ Samples/VueJS-3-SFC/sigcapt/src/components/SignerName.vue
@@ -0,0 +1,35 @@
+/* ********************************************************************************
+   SignerName.vue
+
+   This contains the VUE template for the signatory names - first and last
+
+   Copyright (c) 2022 Wacom Co. Ltd.
+
+***********************************************************************************/
+<script>
+export default
+{
+  data ()
+  {
+    return {
+      fname: "John",
+      lname: "Smith"
+    };
+  }
+};
+</script>
+<template>
+    <table style="padding: 10px 20px;">
+    <tr>
+    <td>Data included in the hash:</td>
+    </tr>
+    <tr>
+        <td>
+        First name: <input type="text" id="fname" v-model="fname"/>
+        </td>
+        <td>
+        Last name: <input type="text" id="lname" v-model="lname"/>
+        </td>
+    </tr>
+    </table>
+</template>
