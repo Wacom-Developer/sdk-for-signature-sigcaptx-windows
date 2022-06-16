af190f05fd0f0fc56bf96a3f1069986c6966fd60
sigsupport <55099853+sigsupport@users.noreply.github.com>
2022-06-16 11:28:32 +0100

    Add files via upload

---
+++ Samples/VueJS-3-SFC/wizpin/src/components/WizOptions.vue
@@ -0,0 +1,28 @@
+/***************************************************************************
+  WizOptions.vue
+
+  This file contains the VUE template for the checkbox option which
+  the user can select to control whether the wizard control window
+  is shown on the PC monitor
+
+  Copyright (c) 2022 Wacom Co. Ltd. All rights reserved.
+
+   v1.0
+
+***************************************************************************/
+<script>
+export default
+{
+  data ()
+  {
+    return {
+      displayWizard: true
+    };
+  }
+};
+</script>
+
+<template>
+    <b>Options: </b>
+    <input type="checkbox" id="chkDisplayWizard" checked="checked" v-model="displayWizard"/>Display Wizard Control Window
+</template>
---
+++ Samples/VueJS-3-SFC/wizpin/src/components/WizPinMain.vue
@@ -0,0 +1,54 @@
+/***************************************************************************
+  WizPinMain.vue
+
+  This file contains the main framework definition for the HTML form plus
+  some short routines used for starting off the sample.
+
+  Copyright (c) 2022 Wacom Co. Ltd. All rights reserved.
+
+   v1.0
+
+***************************************************************************/
+<script>
+/* eslint quotes: ["error", "double"] */
+/* eslint semi: ["error", "always"] */
+/* eslint brace-style: ["error", "allman"] */
+
+import { actionWhenRestarted, startStop } from "./SessionControl.js";
+import WizOptions from "./WizOptions.vue";
+import { userMessage } from "./WizUtils.js";
+
+export default {
+  components: {
+    WizOptions
+  },
+  methods:
+  {
+    bodyonload: function ()
+    {
+      userMessage(" "); // Clear the user message text box
+      actionWhenRestarted();
+    },
+    startWiz: function ()
+    {
+      startStop(this.$refs.wizoptions);
+    }
+  },
+  mounted ()
+  {
+    this.bodyonload();
+  }
+};
+</script>
+
+<template>
+  <div style="width:100%">
+    <br/>
+    <input type="button" id="btnStartStopWizard" value="Start Wizard" style="width:35mm" @click="startWiz" title="Starts/Stops a Wizard Script" />
+    <br/>
+    <br/>
+    <WizOptions ref="wizoptions"/>
+    <br/><br/>
+    <textarea cols="80" rows="40" id="txtDisplay"></textarea>
+  </div>
+</template>
