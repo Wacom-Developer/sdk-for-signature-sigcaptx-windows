7f79fbd304c176ae64ba20a16d4aa48b2b9fd308
sigsupport <55099853+sigsupport@users.noreply.github.com>
2022-06-16 11:21:24 +0100

    Add files via upload

---
+++ Samples/VueJS-3-SFC/wizard/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <page-header/>
+  <wizard-main/>
+</template>
+
+<script>
+import PageHeader from '@/components/PageHeader.vue';
+import WizardMain from '@/components/WizardMain.vue';
+
+export default
+{
+  components:
+  {
+    PageHeader,
+    WizardMain
+  }
+};
+</script>
