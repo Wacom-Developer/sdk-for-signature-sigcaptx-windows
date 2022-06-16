a14c0dbd9bb6e8a4656e66faaa2f5f7017b90265
sigsupport <55099853+sigsupport@users.noreply.github.com>
2022-06-16 11:27:22 +0100

    Add files via upload

---
+++ Samples/VueJS-3-SFC/wizpin/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <page-header/>
+  <wiz-pin-main/>
+</template>
+
+<script>
+import PageHeader from '@/components/PageHeader.vue';
+import WizPinMain from '@/components/WizPinMain.vue';
+
+export default
+{
+  components:
+  {
+    PageHeader,
+    WizPinMain
+  }
+};
+</script>
